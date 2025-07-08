a4b6a26a1ce3113d918a859ed62d1927cb547ef4
zhangwei <zhangwei03@shengwang.cn>
2025-07-08 11:02:05 +0800

    [Android]audio resource copy.

--- Android/APIExample/app/src/main/java/io/agora/api/example/MainApplication.java
+++ Android/APIExample/app/src/main/java/io/agora/api/example/MainApplication.java
@@ -1,9 +1,17 @@
 package io.agora.api.example;
 
 import android.app.Application;
+import android.util.Log;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.annotation.Annotation;
 import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import io.agora.api.example.annotation.Example;
 import io.agora.api.example.common.model.Examples;
@@ -17,10 +25,17 @@ public class MainApplication extends Application {
 
     private GlobalSettings globalSettings;
 
+    private static final String TAG = "MainApplication";
+
     @Override
     public void onCreate() {
         super.onCreate();
         initExamples();
+        try {
+            extractResourceToCache("common_resource.zip");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to init files", e);
+        }
     }
 
     private void initExamples() {
@@ -52,5 +67,57 @@ public class MainApplication extends Application {
             globalSettings = new GlobalSettings();
         }
         return globalSettings;
+    }
+
+
+    private void extractResourceToCache(String zipFileName) throws IOException {
+        String dirName = zipFileName.substring(0, zipFileName.lastIndexOf(".zip"));
+        File resourceDir = new File(getCacheDir(), dirName);
+
+        if (resourceDir.exists()) {
+            Log.d(TAG, "Resources already exist at: " + resourceDir.getAbsolutePath());
+            return;
+        }
+
+        File zipFile = new File(getCacheDir(), zipFileName);
+        // Copy asset to cache
+        try (InputStream input = getAssets().open(zipFileName);
+             FileOutputStream output = new FileOutputStream(zipFile)) {
+            byte[] buffer = new byte[4096];
+            int len;
+            while ((len = input.read(buffer)) > 0) {
+                output.write(buffer, 0, len);
+            }
+        }
+
+        // Unzip
+        try (ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFile))) {
+            ZipEntry entry;
+            byte[] buffer = new byte[4096];
+            while ((entry = zipIn.getNextEntry()) != null) {
+                File newFile = new File(getCacheDir(), entry.getName());
+
+                if (entry.isDirectory()) {
+                    newFile.mkdirs();
+                    continue;
+                }
+
+                File parent = newFile.getParentFile();
+                if (parent != null) {
+                    parent.mkdirs();
+                }
+
+                try (FileOutputStream fos = new FileOutputStream(newFile)) {
+                    int len;
+                    while ((len = zipIn.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+                }
+                long lastModified = entry.getTime();
+                newFile.setLastModified(lastModified);
+            }
+        }
+        zipFile.delete();
+        Log.d(TAG, "Extracted resources to: " + resourceDir.getAbsolutePath());
     }
 }
